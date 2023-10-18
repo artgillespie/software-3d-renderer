84f8599ed65557e568d99bf0ea048235033a7b74
Art <art@tapsquare.com>
2023-10-18 07:11:26 -0700

    commit before no-cglm branch

--- NOTES.md
+++ NOTES.md
@@ -1,7 +1,12 @@
 # TODO
 
-- [x] Clip-space line clipping (Liang-Barsky or Cohen-Sutherland)
-- [ ] Frustum culling
+- [ ] winding order (clockwise)
+- [ ] backface culling w/normal
+- [ ] fill triangle
+- [ ] shading + wireframe
+- [ ] obj loader 
+- [ ] Line + Triangle clipping against near plane + screen bounds
 - [ ] Texture mapping
+- [x] Clip-space line clipping (Liang-Barsky or Cohen-Sutherland)
 
 
--- game.cc
+++ game.cc
@@ -1,9 +1,10 @@
-// #define DEBUG = 1;
+#define DEBUG = 1;
 #include "game.h"
 #include "SDL.h"
 #include "SDL_log.h"
 #include "assert.h"
 #include "cglm/cglm.h"
+#include <vector>
 
 struct state {
   int32_t clear_color;
@@ -30,12 +31,14 @@ struct gfx_point {
   int y;
 } typedef gfx_point;
 
+void load_mesh_from_obj(const char *filename) {}
+
 // clang-format off
 float *cube_mesh =
-    (float[]){-1.0, 1.0, -1.0, 
-              1.0, -1.0, -1.0, 
-              -1.0, -1.0, -1.0,
+    (float[]){-1.0, -1.0, -1.0, 
               -1.0, 1.0, -1.0, 
+              1.0, 1.0, -1.0,
+              -1.0, -1.0, -1.0, 
               1.0, 1.0, -1.0, 
               1.0, -1.0, -1.0,
               // right
@@ -53,12 +56,12 @@ float *cube_mesh =
               -1.0, 1.0, 1.0,
               -1.0, -1.0, 1.0,
               // back
-              -1.0, 1.0, 1.0, 
-              1.0, -1.0, 1.0,
-              -1.0, -1.0, 1.0,
-              -1.0, 1.0, 1.0, 
+              1.0, -1.0, 1.0, 
               1.0, 1.0, 1.0,
-              1.0, -1.0, 1.0,
+              -1.0, 1.0, 1.0,
+              1.0, -1.0, 1.0, 
+              -1.0, 1.0, 1.0,
+              -1.0, -1.0, 1.0,
               // bottom
               -1.0, -1.0, -1.0,
               1.0, -1.0, 1.0,
@@ -85,6 +88,7 @@ inline void gfx_clear(SDL_Surface *surface, int32_t color) {
   }
 }
 
+// in screen coordinate space
 inline void gfx_plot(SDL_Surface *surface, int x, int y, int32_t color) {
   if (x >= surface->w || y >= surface->h || x <= 0 || y <= 0) {
     return;
@@ -93,6 +97,7 @@ inline void gfx_plot(SDL_Surface *surface, int x, int y, int32_t color) {
   pixels[x + y * surface->w] = color;
 }
 
+// in screen coordinate space
 inline void gfx_draw_line(SDL_Surface *surface, vec3 from, vec3 to,
                           int32_t color) {
   // unoptimized [digital differential analyzer
@@ -123,6 +128,8 @@ inline void gfx_draw_line(SDL_Surface *surface, vec3 from, vec3 to,
   }
 }
 
+// TODO: need to pass transforms in individually since we need
+// to clip before the projection transform
 void gfx_draw_triangles(SDL_Surface *surface, float *vertices, int32_t count,
                         mat4 transform, int32_t color) {
   vec4 a;
@@ -176,34 +183,17 @@ inline float wrapf(float v, float min, float max) {
   return v;
 }
 
-void gm_vec2_clamp_and_scale(vec2 v, float min, float max) {
-  float s = -INFINITY;
-  if (v[0] > max) {
-    s = fabs(1.f / v[0]);
-  } else if (v[0] < min) {
-    s = fabs(1.f / v[0]);
-  }
-  if (v[1] > max) {
-    s = fabs(1.f / v[1]);
-  } else if (v[0] < min) {
-    s = fabs(1.f / v[1]);
-  }
-  if (s == -INFINITY) {
-    return;
-  }
-  v[0] *= s;
-  v[1] *= s;
-}
-
 // returns `false` if the line is completely outside clip space
-inline bool gfx_clip_line(vec2 a, vec2 b, float min, float max) {
+inline bool gfx_clip_line(vec3 a, vec3 b, float min, float max) {
   // liang-barsky algorithm https://en.wikipedia.org/wiki/Liang–Barsky_algorithm
   float dx = b[0] - a[0];
   float dy = b[1] - a[1];
+  float dz = b[2] - a[2];
 
-  float p[4] = {-dx, dx, -dy, dy};
+  float p[6] = {-dx, dx, -dy, dy, -dz, dz};
 
-  float q[4] = {a[0] - min, max - a[0], a[1] - min, max - a[1]};
+  float q[6] = {a[0] - min, max - a[0], a[1] - min,
+                max - a[1], a[2] - min, max - a[2]};
 
   for (int i = 0; i < 4; i++) {
     if (p[i] == 0.f && q[i] < 0.f) {
@@ -223,7 +213,6 @@ inline bool gfx_clip_line(vec2 a, vec2 b, float min, float max) {
       u2 = fminf(u2, fminf(1.f, q[i] / p[i]));
     }
   }
-  fprintf(stderr, "clip_line: %.2f, %.2f\n", u1, u2);
   if (u1 > u2) {
     // outside
     return false;
@@ -252,6 +241,14 @@ void gfx_draw_lines_tx(SDL_Surface *surface, float *vertices, mat4 transform,
     glm_vec3_make(vp, b);
     b[3] = 1.f;
     vp += 3;
+    /*
+    fprintf(stderr, "drawing line\n");
+    fprintf(stderr, "A: ");
+    glm_vec3_print(a, stderr);
+    fprintf(stderr, "B: ");
+    glm_vec3_print(b, stderr);
+    */
+
     glm_mat4_mulv(transform, a, a);
     if (a[3] == 0.f) {
       return;
@@ -261,12 +258,18 @@ void gfx_draw_lines_tx(SDL_Surface *surface, float *vertices, mat4 transform,
     if (b[3] == 0.f) {
       return;
     }
-    glm_vec4_divs(b, b[3], b);
-
-    // TODO: clip the line
     if (!gfx_clip_line(a, b, -1., 1.)) {
       return;
     }
+
+    glm_vec4_divs(b, b[3], b);
+    /*
+    fprintf(stderr, "post-transform\n");
+    fprintf(stderr, "A: ");
+    glm_vec3_print(a, stderr);
+    fprintf(stderr, "B: ");
+    glm_vec3_print(b, stderr);
+    */
 
     glm_mat4_mulv(g_state.viewport_tx, a, a);
     glm_mat4_mulv(g_state.viewport_tx, b, b);
@@ -276,18 +279,20 @@ void gfx_draw_lines_tx(SDL_Surface *surface, float *vertices, mat4 transform,
 }
 
 void gm_draw_axes(SDL_Surface *surface, mat4 transform, int32_t color) {
+  /*
   float v2[] = {-10.f, 0.f, 0.f, 10.f, 0.f, 0.f};
   gfx_draw_lines_tx(surface, v2, transform, 1, 0xFFFF0000);
 
   float vertices[] = {0.f, 10.f, 0.f, 0.f, -10.f, 0.f};
   gfx_draw_lines_tx(surface, vertices, transform, 1, 0xFF00FF00);
+  */
 
   float v3[] = {0.f, 0.f, -10.f, 0.f, 0.f, 10.f};
   gfx_draw_lines_tx(surface, v3, transform, 1, 0xFF3333FF);
-  float v4[] = {-1.f, 0.f, -10.f, -1.f, 0.f, 10.f};
-  gfx_draw_lines_tx(surface, v4, transform, 1, 0xFF3333FF);
-  float v5[] = {1.f, 0.f, -10.f, 1.f, 0.f, 10.f};
-  gfx_draw_lines_tx(surface, v5, transform, 1, 0xFF3333FF);
+  // float v4[] = {-1.f, 0.f, -10.f, -1.f, 0.f, 10.f};
+  // gfx_draw_lines_tx(surface, v4, transform, 1, 0xFF3333FF);
+  // float v5[] = {1.f, 0.f, -10.f, 1.f, 0.f, 10.f};
+  // gfx_draw_lines_tx(surface, v5, transform, 1, 0xFF3333FF);
 }
 
 int gm_start() {
@@ -302,6 +307,52 @@ int gm_start() {
   g_state.geometry.z_rotation = 0.f;
   // SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
   return 0;
+}
+
+void gm_debug_tx_pt(mat4 view, mat4 perspective, vec4 a, vec4 b) {
+  fprintf(stderr, "debug point translation\n");
+  fprintf(stderr, "A: ");
+  glm_vec4_print(a, stderr);
+  fprintf(stderr, "B: ");
+  glm_vec4_print(b, stderr);
+
+  fprintf(stderr, "camera pos: ");
+  glm_vec3_print(g_state.camera_pos, stderr);
+  glm_mat4_print(view, stderr);
+
+  glm_mat4_mulv(view, a, a);
+  glm_mat4_mulv(view, b, b);
+
+  fprintf(stderr, "-- post view translation\n");
+  fprintf(stderr, "A: ");
+  glm_vec4_print(a, stderr);
+  fprintf(stderr, "B: ");
+  glm_vec4_print(b, stderr);
+
+  glm_mat4_mulv(perspective, a, a);
+  glm_mat4_mulv(perspective, b, b);
+
+  fprintf(stderr, "-- post perspective translation\n");
+  fprintf(stderr, "A: ");
+  glm_vec4_print(a, stderr);
+  fprintf(stderr, "B: ");
+  glm_vec4_print(b, stderr);
+
+  if (a[3] == 0.f) {
+    fprintf(stderr, "divide by w zero: pt A");
+    return;
+  }
+  if (b[3] == 0.f) {
+    fprintf(stderr, "divide by w zero: pt B");
+    return;
+  }
+  glm_vec4_divs(a, a[3], a);
+  glm_vec4_divs(b, b[3], b);
+  fprintf(stderr, "-- post perspective divide\n");
+  fprintf(stderr, "A: ");
+  glm_vec4_print(a, stderr);
+  fprintf(stderr, "B: ");
+  glm_vec4_print(b, stderr);
 }
 
 int gm_process(SDL_Surface *surface) {
@@ -333,6 +384,10 @@ int gm_process(SDL_Surface *surface) {
   glm_vec3_add(g_state.camera_pos, g_state.camera_vel, g_state.camera_pos);
   glm_translate(view, g_state.camera_pos);
 
+  vec4 ta = {0.f, 0.f, -10.f, 1.f};
+  vec4 tb = {0.f, 0.f, 10.f, 1.f};
+  gm_debug_tx_pt(view, perspective, ta, tb);
+
   glm_translate(g_state.viewport_tx,
                 (vec3){surface->w / 2.f, surface->h / 2.f, 1.0});
   glm_scale(g_state.viewport_tx,
@@ -355,11 +410,8 @@ int gm_process(SDL_Surface *surface) {
   glm_mat4_mul(view, transform, transform);
   glm_mat4_mul(perspective, transform, transform);
 
-  // gfx_draw_triangles(surface, cube_mesh, 12, transform, 0xFF00FFFF);
+  gfx_draw_triangles(surface, cube_mesh, 12, transform, 0xFF00FFFF);
 
-  vec3 test = {0.f, 0.f, 0.f};
-  glm_mat4_mulv(transform, test, test);
-  glm_vec3_print(test, stderr);
   g_state.elapsed_frames++;
   return 0;
 }
